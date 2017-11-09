3465acb4bfbb3ae758318c62ba3b351c22ad3312
LukyamuziB <lukyamuzibenon@gmail.com>
2017-11-10 01:49:19 +0300

    fixed bug in models and included intializers

--- app/models.py
+++ app/models.py
@@ -1,17 +1,26 @@
 from . import db
+# from jksfhj import jksfjk
 
 class Categories(db.Model):
     __tablename__ = 'categories'
     id = db.Column( db.Integer, primary_key = True, autoincrement = True)
     name = db.Column( db.String(50), unique = True)
-    description = db.Column( db.String(200))
+    description = db.Column( db.Text)
+
+    def __init__(self, name, decription):
+        self.name = name
+        self.description = description
 
 
 class Recipes(db.Model):
     __tablename__ = 'recipes'
     id = db.Column(db.Integer, primary_key = True, autoincrement = True)
     name = db.Column(db.String(50), unique = True)
-    description = db.Column(db.String(200))
+    description = db.Column(db.Text)
+
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
 
 
 class User(db.Model):
@@ -21,5 +30,13 @@ class User(db.Model):
     username = db.Column(db.String(50), nullable = False, unique = True)
     email = db.Column(db.String(50), nullable = False)
     password = db.Column(db.String(50), nullable = False)
+
+
+    def __init__(self, name, username, email, password):
+        self.name = name
+        self.username = username
+        self.email = email
+        self.password = password
+
 
 
