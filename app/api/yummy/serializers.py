2ba69103e62d311b28dd57f8928a8f405363a2f1
LukyamuziB <lukyamuzibenon@gmail.com>
2017-11-27 11:37:21 +0300

    created API serializable models

--- app/api/yummy/serializers.py
+++ app/api/yummy/serializers.py
@@ -1,25 +1,24 @@
-from flask_restplus import fields
-from app.api.restplus import api
+from flask_restplus import fields, Resource
+from ... import api
 
 recipe = api.model('Blog post', {
     'id': fields.Integer(readOnly=True, description='recipe unique identifier'),
     'name': fields.String(required=True, description='recipe name'),
     'description': fields.String(required=True, description='A brief description of the recipes'),
-    'recipe steps': fields.String(required = True, descripton = 'Procedure/ Steps of the Recipe'
+    'recipe steps': fields.String(required = True, descripton = 'Procedure/ Steps of the Recipe'),
     'category_id': fields.Integer(attribute='category.id'),
     'category': fields.String(attribute='category.id'),
 })
 
-recipe_collection = api.inherit('Page of blog posts', pagination, {
-    'items': fields.List(fields.Nested(recipe))
-})
+recipe_collection = api.inherit('Recipes Collection', recipe, {
+    'items': fields.List(fields.Nested(recipe))})
 
-category = api.model('Blog category', {
+category = api.model('Recipe Category', {
     'id': fields.Integer(readOnly=True, description='Unqiue category Id for Identity purposes'),
     'name': fields.String(required=True, description='Category name'),
-    'description': fileds.String(required = True, description = 'A brief description of the category')
+    'description': fields.String(required = True, description = 'A brief description of the category')
 })
 
 category_with_recipes = api.inherit('Yummy category with recipes', category, {
-    'category': fields.List(fields.Nested(blog_post))
+    'Recipe': fields.List(fields.Nested(recipe))
 })
