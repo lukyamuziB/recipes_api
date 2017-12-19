bf9dba3ad6f392fff2d210940566034d7e546c2a
Andela <lukyamuzibenon@gmail.com>
2017-12-19 14:47:40 +0300

    api business logic

--- app/api/yummy/utilities.py
+++ app/api/yummy/utilities.py
@@ -1,55 +1,94 @@
-from app.models import Categories, Recipes
+from app.models import Categories, Recipes, User
 from ... import db
 
 
-def create_recipe(data):
-    name = data.get('name')
-    description = data.get('description')
-    category_id = data.get('category_id')
-    recipe_id = data.get('recipe_id')
-    category = Categories.query.filter(Categories.id == category_id).one()
-    recipe = Categories(name, description)
-    db.session.add(recipe)
+def save(data):
+    db.session.add(data)
     db.session.commit()
 
 
+def check_category_exists(category):
+    ctg = categories.query.filter_by(name = category).first()
+    if ctg:
+        return False
+    return True
+
+
+def check_recipe_exists(recipe):
+    rcp = Recipes.query.filter_by(name = recipe).first()
+    if rcp:
+        return False
+    return True
+
+
+def check_user_exists(username, email):
+    if User.query.filter_by(username = username).first() or \
+    User.query.filter_by(email = email).first():
+        return True
+    return False
+
+
+def create_recipe(data, category_id, usr_id):
+    name = data.get('name')
+    description = data.get('description')
+    category = Categories.query.filter_by(id =category_id).first()
+    user = User.query.filter_by(id = usr_id).first()
+    if check_recipe_exists(name):
+         recipe = Recipe(name = name, description = description,
+          category = category, user = user)
+         save(recipe)
+    else:
+        return 0
+
+
 def update_recipe(recipe_id, data):
-    post = Recipes.query.filter(Recipes.id == recipe_id).one()
-    post.title = data.get('title')
-    post.body = data.get('body')
-    category_id = data.get('category_id')
-    post.category = Categories.query.filter(Categories.id == category_id).one()
-    db.session.add(post)
+    recipe = Recipes.query.filter(Recipes.id == recipe_id).first()
+    recipe.name = data.get('name')
+    recipe.description = data.get('description')
     db.session.commit()
 
 
 def delete_recipe(recipe_id):
     recipe = Recipes.query.filter(Recipes.id == recipe_id).one()
-    db.session.delete(recipe)
-    db.session.commit()
+    save(recipe)
 
 
-def create_category(data):
-    category_id = data.get('id')
+def create_category(data, user_id):
     name = data.get('name')
     description = data.get('description')
-
-    category = Category(name)
-    if category_id:
-        category.id = category_id
-
-    db.session.add(category)
-    db.session.commit()
+    user = User.query.filter_by(id = user_id).first()
+    if check_category_exists(name):
+        category = Categories(name = name, 
+        description = description, user = user)
+        save(category)
 
 
 def update_category(category_id, data):
-    category = Category.query.filter(Category.id == category_id).one()
+    category = Category.query.filter_by(id = category_id).first()
     category.name = data.get('name')
-    db.session.add(category)
     db.session.commit()
 
 
 def delete_category(category_id):
-    category = Category.query.filter(Category.id == category_id).one()
-    db.session.delete(category)
-    db.session.commit()
+    category = Category.query.filter_by(id = category_id)
+    save(category)
+
+
+def register_user(data):
+    name = data.get('name')
+    username = data.get('username')
+    email = date.get('email')
+    password = date.get('password')
+    user = User(name = name, username = username, email = email, password = password )
+    db.session.add()
+
+
+def user_login(data):
+    pass
+
+
+def user_logout():
+    pass
+
+
+
