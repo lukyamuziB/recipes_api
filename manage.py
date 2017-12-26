2804f1f85c35ff84b787888cbbcfb67e8a792fcf
= <=>
2017-12-26 10:47:11 +0300

    add commands for migrations, debugging and running tests

--- manage.py
+++ manage.py
@@ -1,8 +1,8 @@
+import os
 from flask_script import Shell, Manager
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Shell, Manager
 from flask_sqlalchemy import SQLAlchemy
-
 
 from config import config
 from app import create_app, db
@@ -19,7 +19,23 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Recipes = Recipes,
+               Categories = Categories)
+
+
+manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
+
+
+@manager.command
+def test():
+    os.system("pytest --cov-report term --cov=app")
+
+
+@manager.command
+def debug_tests():
+    os.system("pytest --ipdb")
 
 
 if __name__ == '__main__':
