92c179e6dece980c73f755b589cc8c8223a9dd01
= <=>
2017-12-31 19:43:33 +0300

    fix for db integrity error

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
-from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.security import generate_password_hash, \
+           check_password_hash
 from random import seed, randint
 import forgery_py
 from sqlalchemy.exc import IntegrityError
@@ -8,13 +9,18 @@ from . import db
 class User(db.Model):
     """user database modeal"""
     __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
-    name = db.Column(db.String(50), nullable = False)
-    username = db.Column(db.String(50), nullable = False, unique = True)
+    id = db.Column(db.Integer, primary_key = True,
+             autoincrement = True)
+    name = db.Column(db.String(50), 
+              nullable = False)
+    username = db.Column(db.String(50), nullable = False,
+                    unique = True)
     email = db.Column(db.String(50), nullable = False)
     password = db.Column(db.String(50), nullable = False)
-    recipes = db.relationship('Recipes', backref = 'user', lazy = 'dynamic')
-    categories = db.relationship('Categories', backref = 'user', lazy = 'dynamic')
+    recipes = db.relationship('Recipes', backref = 'user',
+                  lazy = 'dynamic')
+    categories = db.relationship('Categories', backref = 'user',
+                    lazy = 'dynamic')
 
     """makes the password colum impossible to querry """
     @property
@@ -28,32 +34,35 @@ class User(db.Model):
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
 
-    """ Function to generate fake users for testing the API at a large scale"""
+    """ generates fake users for testing the API at a large scale"""
     @staticmethod
     def data_generator(count=200):
         seed()
-        num = User.query.count()
+        # num = User.query.count()
         for i in range(count):
-            u = User.query.offset(randint(0, num)).first()
-            c = Categories(name=forgery_py.lorem_ipsum.word(),
-            description=forgery_py.lorem_ipsum.sentence(),
-            user= u)
-            db.session.add(c)
+            u = User(name = forgery_py.name.full_name(),
+            username=forgery_py.internet.user_name(True),
+            email=forgery_py.internet.email_address(),
+            password = forgery_py.lorem_ipsum.word())
+            db.session.add(u)
             try:
                 db.session.commit()
-                print("sucessful")
+                # print("sucessful")
             except IntegrityError:
                 db.session.rollback()
-                print("failed")
+                # print("failed")
 
 
 class Categories(db.Model):
     __tablename__ = 'categories'
-    id = db.Column( db.Integer, primary_key = True, autoincrement = True)
+    id = db.Column( db.Integer, primary_key = True,
+                 autoincrement = True)
     name = db.Column( db.String(50), unique = True)
     description = db.Column( db.Text)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable = False)
-    recipes = db.relationship('Recipes', backref = 'category', lazy = 'dynamic')
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'),
+                  nullable = False)
+    recipes = db.relationship('Recipes', backref = 'category',
+                   lazy = 'dynamic')
 
     @staticmethod
     def data_generator(count=200):
@@ -73,11 +82,14 @@ class Categories(db.Model):
 
 class Recipes(db.Model):
     __tablename__ = 'recipes'
-    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    id = db.Column(db.Integer, primary_key = True,
+                     autoincrement = True)
     name = db.Column(db.String(50), unique = True)
     description = db.Column(db.Text)
-    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'), nullable = False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable = False )
+    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'),
+                        nullable = False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'),
+                         nullable = False )
     
     """ generates fake recipes for testing API """
     @staticmethod
@@ -92,8 +104,7 @@ class Recipes(db.Model):
             description = forgery_py.lorem_ipsum.sentence(),
             category = c, user = u)
             db.session.add(r)
-        try:
-            db.session.commit()
-            print("successfully commited")
-        except IntegrityError:
-            db.session.rollback()
+            try:
+                db.session.commit()
+            except IntegrityError:
+                db.session.rollback()
