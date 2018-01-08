31bf5eda5e3b504307e4aa4131ecf6bf2623c1ab
= <=>
2018-01-08 10:28:06 +0300

    FIX error messages and INCLUDE token protection for some routes

--- app/api/yummy/endpoints/categories.py
+++ app/api/yummy/endpoints/categories.py
@@ -1,6 +1,6 @@
-from flask import request
+from flask import request, jsonify
 from flask_restplus import Resource
-from flask_jwt_extended import jwt_required
+from flask_jwt_extended import jwt_required, get_jwt_identity
 
 
 from app.api.yummy.utilities import create_category,\
@@ -12,7 +12,7 @@ from ...restplus import api
 from app.models import Categories
 
 
-ns = api.namespace('Categories', \
+ns = api.namespace('categories', \
   description='Operations related to Recipe Categories')
 
 
@@ -44,16 +44,18 @@ class CategoryCollection(Resource):
     @api.response(201, 'Category successfully created.')
     @api.response(409, 'Conflict, Category already exists')
     @api.expect(category)
+    @jwt_required
     def post(self):
        
         """ Creates a new  category. """
-        user_id = 4
+
+        user_id = get_jwt_identity()
         data = request.json
         try:
             create_category(data, user_id)
-            return '{message: Sucessfuly created category}', 201
+            return jsonify({"Message": "Sucessfuly created category"}), 201
         except ValueError as e:
-            return "{Error: You are creating an already existent Category}",409
+            return jsonify({"Error": "You are creating an already existent Category"}),409
 
 
 
@@ -71,20 +73,29 @@ class CategoryItem(Resource):
     @api.expect(category)
     @jwt_required
     @api.response(204, 'Category successfully updated.')
+    @api.response(404, "Not Found, Category doesn't exist")
+    @api.response(403, "Forbidden, You don't own this category")
     def put(self, id):
         """
         * Updates a category in the Yummy recipes database
         * Specify the ID of the category to modify in the request URL path.
         """
         data = request.json
+        a = get_jwt_identity()
+        print(a)
+        print("dfhgjdg")
         try:
             update_category(id, data)
-            return '{message: Category successfully updated}', 204
+            return jsonify({"Message": "Category successfully updated"}), 200
         except ValueError as e:
-            return "{Error: Can't edit non existent Category}",404
+            return jsonify({"Error": "Can't edit non existent Category"}),404
+        except TypeError as e:
+            return jsonify({"Error": "Can't Edit a Category a you didn't create"}),403
 
 
     @api.response(204, 'Category successfully deleted.')
+    @api.response(404, 'Not Found, Category does not exixt')
+    @api.response(403, "Forbidden, You don't own this category")
     @jwt_required
     def delete(self, id):
         
@@ -92,8 +103,9 @@ class CategoryItem(Resource):
 
         try:
             delete_category(id)
-            return "{Successful: Deleted Category}", 204
+            return jsomnify({"Message": "Deleted Category"}), 200
         except ValueError as identifier:
-            return "{Error: Can't delete non exixtent Category}",404
-        
+            return jsonify({"Error": "Can't delete non exixtent Category"}),404
+        except TypeError as e:
+            return jsonify({"Error": "Can't delete a recipe a you didn't create"}),403
 
