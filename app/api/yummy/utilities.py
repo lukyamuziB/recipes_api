48fa26e28b6cd08c79da89eadd0a3d328288abf1
= <=>
2018-01-08 10:23:39 +0300

    ADD logic for user logout and password reset

--- app/api/yummy/utilities.py
+++ app/api/yummy/utilities.py
@@ -1,8 +1,11 @@
-from app.models import Categories, Recipes, User
+from app.models import Categories, Recipes, User, Blacklist
+from ... import jwt
 from ... import db
 from sqlalchemy.orm.exc import NoResultFound
-from flask_jwt_extended import create_access_token
-from datetime import datetime
+from flask_jwt_extended import ( get_jwt_identity,
+    create_access_token, get_raw_jwt)
+from datetime import datetime, timedelta
+from flask import jsonify
 
 
 def save(data):
@@ -31,6 +34,12 @@ def check_user_exists(username, email):
     return True
 
 
+#picks user id from the token
+def belongs_to_user():
+    usr_id = get_jwt_identity()
+    return usr_id
+
+
 #creates a recipe if it exists
 def create_recipe(data, category_id, usr_id):
     name = data.get('name')
@@ -52,12 +61,14 @@ def update_recipe(recipe_id, data):
     recipe = Recipes.query.filter(Recipes.id == recipe_id).first()
     if recipe is None:
         raise ValueError
+    elif belongs_to_user() != recipe.user.id:
+        raise TypeError
     else:    
         name = data.get('name')
         recipe.name = name if name is not None else recipe.name
         description = data.get('description')
         recipe.description = description if description is not None else recipe.description
-        # recipe.modified = datetime.utcnow
+        recipe.modified = datetime.now()
         db.session.commit()
 
 
@@ -66,12 +77,14 @@ def delete_recipe(recipe_id):
     recipe = Recipes.query.filter_by(id = recipe_id).first()
     if recipe is None:
         raise ValueError
+    elif belongs_to_user != recipe.user.id:
+        raise TypeError    
     else:
         db.session.delete(recipe)
         db.session.commit()
 
 
-#creates a new category or raises  a value error if category already exists
+#creates a new category if it doesn't exist yet
 def create_category(data, user_id):
     name = data.get('name')
     description = data.get('description')
@@ -89,12 +102,14 @@ def update_category(category_id, data):
     category = Categories.query.filter_by(id = category_id).first()
     if category is None:
         raise ValueError
+    elif belongs_to_user() != category.user.id:
+        raise TypeError 
     else:
         name = data.get('name')
         category.name = name if name is not None else category.name
         description = data.get('description')
         category.description = description if description is not None else category.description
-        # recipe.modified = datetime.utcnow
+        category.modified = datetime.now()
         db.session.commit()
 
 
@@ -103,6 +118,8 @@ def delete_category(category_id):
     category = Categories.query.filter_by(id = category_id).first()
     if category is None:
         raise ValueError
+    elif belongs_to_user != category.user.id:
+        raise TypeError 
     else:
         db.session.delete(category)
         db.session.commit()
@@ -116,12 +133,12 @@ def register_user(data):
     password = data.get('password')
     if check_user_exists(username, email):
         user = User(name = name, username = username, email = email, password = password )
-        db.session.add(user)
-        db.session.commit()
+        save(user)
     else:
         raise ValueError
 
 
+#logs in a registered user and creates a token
 def user_login(data):
     username = data.get('username')
     password = data.get('password')
@@ -130,17 +147,47 @@ def user_login(data):
         raise NoResultFound
     else:
         if user.verify_password(password):
-            access_token = create_access_token(identity = user.id)
+            access_token = create_access_token(identity = user.id, expires_delta = timedelta(days=7))
             return access_token
         else:
             raise ValueError
     return access_token
     
-        
 
-    
-    
-
-
+#blackklists a token
 def user_logout():
-    pass
+    jti = get_raw_jwt()['jti']
+    blacklist = Blacklist(token = jti)
+    db.session.add(blacklist)
+    db.session.commit()
+
+
+#resets a user's password
+def reset_password(data, id):
+    user = User.query.filter_by(id = id).first()
+    password = data.get('password')
+    user.password = password
+
+
+#changes a user's username
+def change_username(date, id):
+    user = User.query.filter_by(id = id).first()
+    username = data.get('username')
+    user.username = username
+
+
+@jwt.token_in_blacklist_loader
+def check_if_token_in_blacklist(decrypted_token):
+    """ Call back function that checks if a the token is valid on all the
+        endpoints that require a token
+    """
+    jti = decrypted_token['jti']
+
+    if Blacklist.query.filter_by(token=jti).first() is None:
+        return False
+    return True
+
+
+@jwt.revoked_token_loader
+def my_revoked_token_callback():
+    return jsonify({'message': 'You must be logged in to access this page'})
