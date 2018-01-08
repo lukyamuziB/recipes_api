98ad198f2bc72a3d546fae68167c2f25a20396a5
= <=>
2018-01-08 10:22:51 +0300

    ADD routes for password reset and logout

--- app/api/yummy/endpoints/user.py
+++ app/api/yummy/endpoints/user.py
@@ -1,13 +1,24 @@
 from flask import request
 from flask_restplus import Resource
 from sqlalchemy.orm.exc import NoResultFound
+from flask_jwt_extended import (
+    jwt_required, get_jwt_identity
+)
 
-from app.api.yummy.utilities import register_user, user_login, user_logout
-from app.api.yummy.serializers import users, usr
+from app.api.yummy.utilities import (
+    register_user, user_login, user_logout,
+    reset_password, change_username
+    
+)
+from app.api.yummy.serializers import (
+    users, usr,
+    users_password_reset, username_reset
+)
+
 from ...restplus import api
 from app.models import User
 
-ns = api.namespace('Users', description='Operations on User Authentication')
+ns = api.namespace('auth', description='Operations on User Authentication')
 
 
 @ns.route('/register')
@@ -39,11 +50,51 @@ class UserLogin(Resource):
         try:
             a = user_login(data) 
             print(a)
-            return "{\nSuccess: user Successfuly loged in\nToken: + a\n}",200
+            return "{\nSuccess: user Successfuly loged in\nToken: a\n}",200
         except NoResultFound as e:
             return "{Error: Username is not Registered}",404
         except ValueError as e:
             return "{Error: Wrong Password}",404
+
+@ns.route('/reset_password')
+class UserPasswordReset(Resource):
+
+    @api.response(200, 'Password Successfully Reset')
+    @api.expect(users_password_reset)
+    @jwt_required
+    def put(self):
+        """ Resets a User's password """
+        
+        data = request.json
+        id = get_jwt_identity()
+        reset_password(data, id)
+        return "{Successful: Password successfully reset}",200
+
+
+@ns.route('/change_username')
+class UsernameReset(Resource):
+
+    @api.expect(username_reset)
+    @api.response(200, 'Username Successfully changed')
+    @jwt_required
+    def put(self):
+        """Resets a user's username """
+
+        data = request.json
+        id = get_jwt_identity
+        change_username(data, id)
+        return "{Successful: Username Successfully changed}",200
+
+@ns.route('/logout')
+class UserLogout(Resource):
+    @api.response(200, "Successfully loged out")
+    @jwt_required
+    def delete(self):
+        """ logs out a user """
+        user_logout()
+        return "{Message: Successfully loged out}",200
+
+
         
 
 
