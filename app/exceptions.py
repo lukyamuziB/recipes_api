f208a17ceac817634e4020401c11018afcd36007
= <=>
2018-01-16 16:36:51 +0300

    Increase custom exceptions

--- app/exceptions.py
+++ app/exceptions.py
@@ -1,5 +1,5 @@
 
-""" custom exceptions to catch unwanted scenarios"""
+#custom exceptions to catch unwanted scenarios
 
 class ResourceAlreadyExists(Exception):
     pass
@@ -18,3 +18,21 @@ class EmailEmpty(Exception):
 
 class NameEmpty(Exception):
     pass
+
+class EmptyField(Exception):
+    pass
+
+class WrongPassword(Exception):
+    pass
+
+class EmptyDescription(Exception):
+    pass
+
+class UsernameFormatError(Exception):
+    pass
+
+class PasswordFormatError(Exception):
+    pass
+
+class EmailFormatError(Exception):
+    pass 
