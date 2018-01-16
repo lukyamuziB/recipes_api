fcc3eaf94bcb223a0c87c4e1cbceaa7a0a9e4848
= <=>
2018-01-16 16:32:06 +0300

    ADD regex validation for fields

---
+++ app/validators.py
@@ -0,0 +1,20 @@
+import re
+
+def validate_username(username):
+    username_pattern = re.compile(r'^[a-zA-Z_]+([a-zA-Z0-9]{1,10})$')
+    if username_pattern.match(username):
+        return True
+    return False
+
+def validate_email(email):
+    email_pattern = re.compile(
+        r'(^[a-zA-Z0-9_.]+@[a-zA-Z0-9]+\.[a-zA-Z0-9.]+$)')
+    if email_pattern.match(email):
+        return True
+    return False
+
+def validate_password(password):
+    password_pattern = re.compile(r'^\w{6,25}$')
+    if password_pattern.match(password):
+        return True
+    return False
